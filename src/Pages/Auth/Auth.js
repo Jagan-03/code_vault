7344179971c5d7989e63333968138ad5d4d91ab2
Jaganath Ezhilarasu <jaganmj324@gmail.com>
2021-12-27 14:00:16 +0530

    reinitialized amplify app

--- src/Pages/Auth/Auth.js
+++ src/Pages/Auth/Auth.js
@@ -1,28 +1,11 @@
 import React from 'react'
-// import { Auth } from "aws-amplify";
-// import { useHistory } from 'react-router-dom';
+import { Auth } from "aws-amplify";
 
 const AuthScreen = () => {
 
-    // const history = useHistory();;
-
-    // const googleSignIn = async () => {
-    //     try {
-    //         await Auth.federatedSignIn({ provider: 'Google'});
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    //   }
-
-    //   const checkUser = async () => {
-    //       const user = await Auth.currentAuthenticatedUser();
-    //       console.log(user); 
-    //       console.log(user.getUsername()); 
-    //   }
-
     return (
         <div className="authScreen d-flex w-100 align-items-center justify-content-center">
-            {/* <button className="btn btn-dark" onClick={() => Auth.federatedSignIn()}>Sign-in</button> */}
+            <button className="btn btn-dark" onClick={() => Auth.federatedSignIn()}>Sign-in</button>
         </div>
     )
 }
